7d2cd04342eb9d8bbb18b324c7efa046d0ca682c
Abass Ibrahim <abassdibrahim@gmail.com>
2022-04-09 06:34:15 +0100

    webapp scroll effect

--- webapp/app.py
+++ webapp/app.py
@@ -14,40 +14,5 @@ app = Flask(__name__)
 val_summary_DataFrame = pd.read_csv('data/NLP_summary_df.csv')
 
 @app.route("/")
-def chart1():
-    greet = greeting('yo')
-    make_plot(val_summary_DataFrame)
-    return render_template('index.html', message = greet)
-
-def greeting(text):
-    return text
-
-def make_plot(df):
-    fig = px.bar(
-        df,
-        x = 'classifiers',
-        y = 'val_accuracy',
-        hover_data = ['val_recall', 'val_precision', 'val_f1'],
-        color = 'val_accuracy',
-        color_continuous_scale = px.colors.sequential.matter,
-        title = 'NLP model summary',
-        height = 720,
-        width = 1280
-    )
-
-    fig.add_hline(
-        y = 0.63,
-        line_dash = 'dot',
-        line_color = 'black'
-    )
-
-    fig.update_layout(
-        yaxis = dict(
-            title_text="Val Accuracy (%)"
-        ),
-        xaxis = dict(
-            title_text = 'Classifiers'
-        )
-    )
-
-    fig.show()
+def index():
+    return render_template('index.html')
