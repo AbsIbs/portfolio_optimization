9e40e79f8c3474a9a69b43c5edd65642a2718d0e
Abass Ibrahim <abassdibrahim@gmail.com>
2022-04-18 09:28:02 +0100

    rl report done

--- webapp/app.py
+++ webapp/app.py
@@ -12,15 +12,11 @@ from nltk.stem.wordnet import WordNetLemmatizer
 import plotly
 import plotly.express as px
 from plotly.io import to_json
+import plotly.graph_objects as go
 import json
 import requests
 
 app = Flask(__name__)
-
-@app.before_first_request
-def DOW_df():
-    
-
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -34,14 +30,47 @@ def cb(endpoint):
         return "Bad endpoint", 400
 
 def gm(stock, period, interval):
-    data = yf.download(stock, period=period, interval=interval)
-    company_name = yf.Ticker(stock).info['longName']
-    fig = px.area(
-        data.reset_index(),
-        x = 'Date',
-        y = 'Adj Close',
-        hover_data = ['Open', 'High', 'Low', 'Volume'],
-        title = company_name
-    )
+    st = yf.Ticker(stock)
+    data = st.history(period=period, interval=interval)
+    st_info = st.info
+    company_name = st_info['longName']
+    company_sector = st_info['sector']
+    company_ROI = round(((100 / data['Close'].iloc[0]) * data['Close'].iloc[-1]) - 100, 2)
+    start_date = str(data.index[0])[:-9]
+    end_date = str(data.index[-1])[:-9]
+    data['ROI'] = data['Close'].pct_change()
+    company_std = round(data['ROI'].dropna().std(), 2)
+
+    def color(ROI):
+        if ROI > 0:
+            return 'green'
+        else:
+            return 'red'
+
+    x = data.reset_index()['Date']
+    y = data.reset_index()['Close']
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=x, y=y,
+        fill='tozeroy',
+        mode='lines',
+        line_color=color(company_ROI),
+        hoverinfo = 'x+y'
+        ))
+    fig.update_layout(title_text = f'Period: {start_date} to {end_date}')
+    fig.update_xaxes(title_text="Date")
+    fig.update_yaxes(title_text="Closing Price ($)")
+        
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    return graphJSON
+
+    gm_dict = {
+        'graphJson': graphJSON,
+        'companyName': company_name,
+        'companySector': company_sector,
+        'companyROI': company_ROI,
+        'companyStd': company_std
+        }
+
+    gm_dict = json.dumps(gm_dict)
+
+    return gm_dict
